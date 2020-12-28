5c50dac8abc14c718c9fba8ca67b3c815c802d8d
Khanh <tran_khanh@outlook.com>
2020-12-28 14:38:33 +0700

    update

---
+++ src/helpers/safeHTMLElements.js
@@ -0,0 +1,7 @@
+import ExecutionEnvironment from "exenv";
+const EE = ExecutionEnvironment;
+
+const SafeHTMLElement = EE.canUseDOM ? window.HTMLElement : {};
+export const canUseDOM = EE.canUseDOM;
+
+export default SafeHTMLElement;
